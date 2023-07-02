0433d22a2078339f2fa6bb4d9a1b8d92da70a0e6
George Kakarlis <georgekakarlis@yahoo.com>
2023-07-02 09:44:05 +0200

    added the changes on port

--- go-fiber-server/db/redis.go
+++ go-fiber-server/db/redis.go
@@ -1,13 +1,22 @@
 package db
 
 import (
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 func CreateRedisConnection() *redis.Client {
+
+	PORT := os.Getenv("REDIS_PORT")
+	if PORT == "" {
+		PORT = "6379" // Default port if REDIS_PORT environment variable is not set
+	}
+
+
 	rdb := redis.NewClient(&redis.Options{
-		// TODO: switch the port to an env variable
-		Addr:     "localhost:6379",
+		// TODO: switch the port to an env variable ✅
+		Addr:     "localhost" + PORT,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
