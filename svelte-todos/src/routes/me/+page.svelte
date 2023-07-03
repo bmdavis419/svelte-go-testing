59484f0b163185dfb8ddda2866ac5dc58d2a9748
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-07-03 16:17:35 -0400

    todos

--- svelte-todos/src/routes/me/+page.svelte
+++ svelte-todos/src/routes/me/+page.svelte
@@ -7,6 +7,7 @@
     <h1>Welcome {data.props.user.FirstName}!</h1>
     <p>Here is your full payload:</p>
     <pre>{JSON.stringify(data.props, null, 2)}</pre>
+    <a href="/">View Todos</a>
     <a href="/sign-out">Log Out</a>
   </main>
 {/if}
